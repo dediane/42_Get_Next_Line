9299f3f070c852fb776f4c885494c06739d33320
dediane <dianedecourt@gmail.com>
2020-12-17 20:17:05 +0100

    modifications of variable name

--- get_next_line.c
+++ get_next_line.c
@@ -6,47 +6,47 @@
 /*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 19:50:43 by ddecourt          #+#    #+#             */
-/*   Updated: 2020/12/17 19:59:14 by ddecourt         ###   ########.fr       */
+/*   Updated: 2020/12/17 20:14:06 by ddecourt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		getline(char **mark, char *buf, int size)
+int		getline(char **tmp_buffer, char *buf, int size)
 {
 	char	*tmp;
 
 	buf[size] = '\0';
-	if (0 != *mark)
+	if (0 != *tmp_buffer)
 	{
-		tmp = ft_strjoin(*mark, buf);
-		free(*mark);
+		tmp = ft_strjoin(*tmp_buffer, buf);
+		free(*tmp_buffer);
 	}
 	else
 		tmp = ft_strndup(buf, size);
-	*mark = tmp;
-	if (ft_strchr(*mark, '\n'))
+	*tmp_buffer = tmp;
+	if (ft_strchr(*tmp_buffer, '\n'))
 		return (1);
 	return (0);
 }
 
-int		check(char **mark, char **line)
+int		check(char **tmp_buffer, char **line)
 {
 	char	*ptr;
 	char	*tmp;
 
-	if ((ptr = ft_strchr(*mark, '\n')))
+	if ((ptr = ft_strchr(*tmp_buffer, '\n')))
 	{
-		*line = ft_strndup(*mark, ptr - *mark);
+		*line = ft_strndup(*tmp_buffer, ptr - *tmp_buffer);
 		tmp = ft_strndup(ptr + 1, ft_strlen(ptr + 1));
-		free(*mark);
-		*mark = tmp;
+		free(*tmp_buffer);
+		*tmp_buffer = tmp;
 		return (1);
 	}
 	else
 	{
-		*line = *mark;
-		*mark = 0;
+		*line = *tmp_buffer;
+		*tmp_buffer = 0;
 		return (0);
 	}
 }
@@ -54,17 +54,17 @@ int		check(char **mark, char **line)
 int		get_next_line(int fd, char **line)
 {
 	int		size;
-	char	*mark[2048];
+	char	*tmp_buffer[2048];
 	char	buf[BUFFER_SIZE + 1];
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || !line)
 		return (-1);
 	while ((size = read(fd, buf, BUFFER_SIZE)) >= 0)
 	{
-		if (getline(&mark[fd], buf, size) || size == 0)
+		if (getline(&tmp_buffer[fd], buf, size) || size == 0)
 			break ;
 	}
 	if (size < 0)
 		return (-1);
-	return (check(&mark[fd], line));
+	return (check(&tmp_buffer[fd], line));
 }
