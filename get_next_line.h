1d95b5bcb4e10a865f8bfeca07821ad8fe5474d8
dediane <dianedecourt@gmail.com>
2020-12-17 19:03:35 +0100

    fixing header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,23 +6,24 @@
 /*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 19:50:54 by ddecourt          #+#    #+#             */
-/*   Updated: 2020/12/17 13:10:29 by ddecourt         ###   ########.fr       */
+/*   Updated: 2020/12/17 19:03:10 by ddecourt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <unistd.h>
+# include <stdlib.h>
+
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 1
 #endif
 
-# include <unistd.h>
-# include <stdlib.h>
-# endif
-
 char	*ft_strndup(const char *s1, int n);
 char	*ft_strchr(const char *s, int c);
 char 	*ft_strjoin(const char *s1, char const *s2);
-int	ft_strlen(const char *str);
+int		ft_strlen(const char *str);
+int		get_next_line(int fd, char **line);
 
+#endif
